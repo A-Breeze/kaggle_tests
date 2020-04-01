97028e1ca02e1b578d705d2138f738b5257c99c4
A-Breeze <alexander.breeze@yahoo.com>
2020-04-01 17:31:41 +0100

    Remove tree() function because it is provided by another script for the notebook, not from this repo

--- mods_for_kaggle/utils.py
+++ mods_for_kaggle/utils.py
@@ -1,4 +1,4 @@
-""""Useful functions to use in the notebook 'Try_out_Kaggle'"""
+﻿"""Useful functions to use in the notebook 'Try_out_Kaggle'"""
 
 #########
 # Setup #
@@ -7,43 +7,10 @@
 __version__ = '0.2.0'  # Useful for checks when running the notebook
 
 # Import built-in modules
-from pathlib import Path
 from urllib.request import urlopen
 
 # Import external modules
 # [None yet]
-
-#################################
-# Visualise directory structure #
-#################################
-def tree(dir_path: Path, prefix: str=''):
-    """
-    A recursive generator, given a directory Path object
-    will yield a visual tree structure line by line
-    with each line prefixed by the same characters
-    
-    Adapted from here: <https://stackoverflow.com/a/59109706>
-    
-    Examples:
-        >>> for line in tree(Path.home()):
-        >>>      print(line)
-    """
-    # prefix components:
-    space =  '    '
-    branch = '│   '
-    # pointers:
-    tee =    '├── '
-    last =   '└── '
-    
-    contents = list(dir_path.iterdir())
-    # contents each get pointers that are ├── with a final └── :
-    pointers = [tee] * (len(contents) - 1) + [last]
-    for pointer, path in zip(pointers, contents):
-        yield prefix + pointer + path.name
-        if path.is_dir(): # extend the prefix and recurse:
-            extension = branch if pointer == tee else space 
-            # i.e. space because last, └── , above so no more |
-            yield from tree(path, prefix=prefix+extension)
 
 ##############################
 # Interact with the internet #
